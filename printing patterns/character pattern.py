ee5eec958f831fee702028ee349388de2e51ab50
Hemanth-sagar <hemanthsagar44@gmail.com>
2022-01-13 01:07:03 +0530

    second commit

--- printing patterns/character pattern.py
+++ printing patterns/character pattern.py
@@ -2,8 +2,10 @@ n = int(input())
 i = 1
 while i<=n:
     j = 1
+    # ord returns ASCII value of argument uh passed
     x = ord('A')
     while j <= i:
+        #chr returns character of value --> chr(65)=='A'
         print(chr(x),end=" ")
         j=j+1
         x=x+1
