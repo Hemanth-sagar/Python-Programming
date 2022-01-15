b85935ced3553b823801bf451e079f1e9dfd7d7f
Hemanth-sagar <hemanthsagar44@gmail.com>
2022-01-15 10:23:17 +0530

    initial commit

---
+++ printing patterns/number patterns.py
@@ -0,0 +1,23 @@
+#pattern 2
+'''
+1
+1 1
+2 0 2
+3 0 0 3
+'''
+n = int(input())
+i = 1
+p = 2
+while i<= n:
+    j = 1
+    while j <= i:
+        p = i - 1
+        if i == 1 or i == 2:
+            print("1 ",end="")
+        elif j == 1 or j == i:
+            print(p,end=" ")
+        else:
+            print("0 ",end="")
+        j +=1
+    i +=1
+    print()
