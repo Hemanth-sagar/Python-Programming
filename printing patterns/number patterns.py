70b07a324017a3acd9f26e4d1d4703601dc59afe
Hemanth-sagar <hemanthsagar44@gmail.com>
2022-01-15 10:56:23 +0530

    updating

--- printing patterns/number patterns.py
+++ printing patterns/number patterns.py
@@ -21,3 +21,29 @@ while i<= n:
         j +=1
     i +=1
     print()
+
+ # pattern 3
+input:
+    n = 4
+output:
+1 
+1 1 
+1 2 1 
+1 2 2 1 
+
+n = int(input())
+i = 1
+p = 2
+while i<= n:
+    j = 1
+    while j <= i:
+        p = i - 1
+        if i == 1 or i == 2:
+            print("1 ",end="")
+        elif j == 1 or j == i:
+            print("1",end=" ")
+        else:
+            print("2 ",end="")
+        j +=1
+    i +=1
+    print()
