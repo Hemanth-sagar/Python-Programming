7b4cf9f1d7e6ed15e38522b582ff0b34091db766
Hemanth-sagar <hemanthsagar44@gmail.com>
2022-01-28 22:27:54 +0530

    substring of len K

---
+++ Problems/strings2.py
@@ -0,0 +1,13 @@
+Given string of s and a number k.Print all the substring of s of length k
+input:
+  prog 2
+output:
+  pr ro og
+
+s=input()
+n=int(input())
+l=[]
+for i in range(len(s)+1):
+  for j in range(i+1,len(s)+1):
+    l.append(s[i:j])
+print(l)
